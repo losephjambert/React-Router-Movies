2a1a3aeea2e77c1236926526b4a760a0e5d6f8cb
Joe <josephc.lambert@gmail.com>
2019-09-16 13:32:33 -0700

    adds movies/:id component and renders link for it.

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import axios from 'axios';
 
 const MovieList = props => {
-  const [movies, setMovies] = useState([])
+  const [movies, setMovies] = useState([]);
   useEffect(() => {
     const getMovies = () => {
       axios
@@ -13,35 +14,37 @@ const MovieList = props => {
         .catch(error => {
           console.error('Server Error', error);
         });
-    }
-    
+    };
+
     getMovies();
   }, []);
-  
+
   return (
-    <div className="movie-list">
+    <div className='movie-list'>
       {movies.map(movie => (
         <MovieDetails key={movie.id} movie={movie} />
       ))}
     </div>
   );
-}
+};
 
 function MovieDetails({ movie }) {
   const { title, director, metascore, stars } = movie;
   return (
-    <div className="movie-card">
-      <h2>{title}</h2>
-      <div className="movie-director">
+    <div className='movie-card'>
+      <Link to={`/movies/${movie.id}`}>
+        <h2>{title}</h2>
+      </Link>
+      <div className='movie-director'>
         Director: <em>{director}</em>
       </div>
-      <div className="movie-metascore">
+      <div className='movie-metascore'>
         Metascore: <strong>{metascore}</strong>
       </div>
       <h3>Actors</h3>
 
       {stars.map(star => (
-        <div key={star} className="movie-star">
+        <div key={star} className='movie-star'>
           {star}
         </div>
       ))}
